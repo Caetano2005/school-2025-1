ae4964b5a9a4427638a21eb9e806d9e7b136123c
Caetano Pavan <caetanopavan0@gmail.com>
2025-05-19 20:47:20 -0300

    school

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header"
+import Banner from "./components/Banner"
+import TitleCourses from "./components/TitleCourses"
+import Courses from "./components/Courses"
+import Footer from "./components/Footer"
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <Banner/>
+      <TitleCourses/>
+      <Courses/>
+      <Footer/>
+      
+    </div>
+  )
+}
