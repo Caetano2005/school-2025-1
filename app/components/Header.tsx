a1df6b5099f64af33f66a99d6c214dfdfef0df18
Caetano Pavan <caetanopavan0@gmail.com>
2025-05-20 22:10:14 -0300

    aula 14

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,10 +7,10 @@ export default function Header() {
         <h4 className="mb-2  font-bold text-xl">🏠ESCOLA DE INFORMÁTICA📘</h4>
       </div>
       <nav className="space-x-2">
-        <a href="">Home</a>
-        <a href="/pages/curso_html.html">Curso de HTML</a>
-        <a href="">Curso CSS</a>
-        <a href="">Curso JS</a>
+        <a href="/">Home</a>
+        <a href="/courses/html">Curso de HTML</a>
+        <a href="/courses/css">Curso CSS</a>
+        <a href="/courses/js">Curso JS</a>
         <a href="#">Contato</a>
       </nav>
     </header>
---
+++ app/courses/js/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/app/components/Footer"
+import Header from "@/app/components/Header"
+
+export default function js() {
+    return (
+        <div>
+        <Header/>
+        <h1>Curso js</h1>
+        <Footer/>
+        </div>
+    )
+}
